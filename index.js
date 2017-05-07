7cad8c2f4f7874dc2e8caf27bdff2690b04e4835
cyberblast <git@cyberblast.org>
2017-05-07 14:07:41 +0200

    added Flags & events

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ module.exports = mod;
 mod.dependencies = [];
 mod.install = function(){
     context.requiresMemory = false;
-    context.memoryPartitions = ['objects', 'volatile', 'rooms', 'hostiles'];
+    context.memoryPartitions = ['objects', 'volatile', 'rooms', 'hostiles', 'flags'];
     
     context.defaultValue('USERNAME', 'unknown');
 
     context.inject(global, 'extension.global');
     context.load('extension.misc').extend();
+    context.inject(Flag, 'extension.flag');
     context.inject(Room, 'extension.room');
+    context.load('main');
 };
